74f0bb35c60078dba2dcf8547b89bb76fecdfe6b
yang <2546467315@qq.com>
2021-08-27 17:31:21 +0800

    deploy

---
+++ precache-manifest.15fdd12b27861c007cdceaccdfb0fcd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2788638d8566aec3caf0",
+    "url": "/lightning-website/css/app.5a1b977b.css"
+  },
+  {
+    "revision": "911ef4a8f82dc62de3d06ba0fa450706",
+    "url": "/lightning-website/index.html"
+  },
+  {
+    "revision": "2788638d8566aec3caf0",
+    "url": "/lightning-website/js/app.77144fc7.js"
+  },
+  {
+    "revision": "403cfe44b111507760bf",
+    "url": "/lightning-website/js/chunk-vendors.6598c70e.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/lightning-website/manifest.json"
+  },
+  {
+    "revision": "2fc7f4e879afd3c92702c3db089ac2bb",
+    "url": "/lightning-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lightning-website/robots.txt"
+  }
+]);
